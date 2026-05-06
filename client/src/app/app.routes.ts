8916502f2336a3104721c225f541ac50b4f3425a
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2026-05-06 17:40:17 +0700

    init project structure

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home-page/home-page').then(m => m.HomePage),
+        title: 'Home Page'
+    },
+    {
+        path: 'account',
+        loadComponent: () => import('./pages/account/account').then(m => m.Account),
+        title: 'Account'
+    },
+    {
+        path: 'classes',
+        loadComponent: () => import('./pages/classes/classes').then(m => m.Classes),
+        title: 'Classes'
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact').then(m => m.Contact),
+        title: 'Contact'
+    },
+    {
+        path: 'reviews',
+        loadComponent: () => import('./pages/reviews/reviews').then(m => m.Reviews),
+        title: 'Reviews'
+    }
+];
