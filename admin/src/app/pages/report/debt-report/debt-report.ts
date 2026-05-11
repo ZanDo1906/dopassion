ac3bdc2fa54b7bfdc087f787f9f510207552c049
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2026-05-11 16:51:08 +0700

    haonf thành

---
+++ admin/src/app/pages/report/debt-report/debt-report.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debt-report',
+  imports: [],
+  templateUrl: './debt-report.html',
+  styleUrl: './debt-report.css',
+})
+export class DebtReport {
+
+}
---
+++ admin/src/app/pages/report/student-report/student-report.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-report',
+  imports: [],
+  templateUrl: './student-report.html',
+  styleUrl: './student-report.css',
+})
+export class StudentReport {
+
+}
