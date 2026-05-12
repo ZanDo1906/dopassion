beeb60c540a7da468483f24a5a1c6569c2c08253
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2026-05-13 00:29:45 +0700

    thêm form-dtalog

---
+++ admin/src/app/components/form-dialog/form-dialog.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './form-dialog.html',
+  styleUrls: ['./form-dialog.css']
+})
+export class GridFormDialog {
+
+  @Input() title: string = '';
+
+  @Input() sections: any[] = [];
+
+  @Input() columns: number = 3;
+
+  @Output() close = new EventEmitter<void>();
+
+  @Output() submit = new EventEmitter<any>();
+
+  formData: any = {};
+
+  onSubmit() {
+    this.submit.emit(this.formData);
+  }
+
+}
--- admin/src/app/pages/courses/classes/classes.ts
+++ admin/src/app/pages/courses/classes/classes.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Class } from '../../../services/class';
+import { iClass } from '../../../interfaces/class';
+import { GridFormDialog } from '../../../components/form-dialog/form-dialog';
 
 @Component({
   selector: 'app-classes',
-  imports: [],
+  imports: [CommonModule, GridFormDialog],
   templateUrl: './classes.html',
   styleUrl: './classes.css',
 })
-export class Classes {
+export class Classes implements OnInit {
+  classes: iClass[] = [];
 
+  isDialogOpen = false;
+  dialogSections = [
+    {
+      title: 'I. Thông tin Khóa học',
+      fields: [
+        { label: 'Mã lớp', name: 'Mã lớp', type: 'text', disabled: true },
+        { label: 'Tên lớp', name: 'Tên lớp', type: 'text', required: true },
+        { label: 'Khung giờ', name: 'Khung giờ', type: 'text', required: true },
+        { label: 'Ngày bắt đầu', name: 'Ngày bắt đầu', type: 'date', required: true },
+        { label: 'Ngày kết thúc', name: 'Ngày kết thúc', type: 'date', required: true },
+        { label: 'Mã khóa', name: 'Mã khóa', type: 'text' },
+        { label: 'Tên khóa học', name: 'Tên khóa học', type: 'text' },
+        { label: 'STT', name: 'STT', type: 'number' }
+      ]
+    },
+    {
+      title: 'II. Thông tin Giáo viên',
+      fields: [
+        { label: 'Chi nhánh', name: 'Chi nhánh', type: 'select', required: true, options: [{ label: 'Chọn chi nhánh...', value: '' }] },
+        { label: 'Giảng viên', name: 'Giảng viên', type: 'select', required: true, options: [{ label: 'Chọn giảng viên...', value: '' }] },
+        { label: 'Mã nhân viên', name: 'Mã nhân viên', type: 'text', disabled: true }
+      ]
+    }
+  ];
+
+  constructor(private classService: Class) { }
+
+  ngOnInit() {
+    this.classService.getClass().subscribe((data) => {
+      this.classes = data;
+    });
+  }
+
+  openDialog() {
+    this.isDialogOpen = true;
+  }
+
+  closeDialog() {
+    this.isDialogOpen = false;
+  }
+
+  onSubmitDialog(data: any) {
+    console.log('Dữ liệu lớp học được thêm:', data);
+    this.isDialogOpen = false;
+  }
 }
