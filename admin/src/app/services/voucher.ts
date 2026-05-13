3f2cb9afdd2fcafc94e8ca0417cfc38955ef6a8c
nhan1665 <nhan10042020@gmail.com>
2026-05-13 21:40:59 +0700

    add view voucher, refund - Nhan

--- admin/src/app/services/voucher.ts
+++ admin/src/app/services/voucher.ts
@@ -1,15 +1,27 @@
-import { iVoucher } from './../interfaces/voucher';
 import { Injectable } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import { iVoucher } from '../interfaces/voucher';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Voucher {
-  url: string = 'assets/mock-data-json/voucher.json';
-  constructor(private http: HttpClient) { }
-  getVoucher(): Observable<iVoucher[]> {
-    return this.http.get<iVoucher[]>(this.url);
+
+export class VoucherService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getVoucher() {
+
+    return this.http.get<iVoucher[]>(
+
+      'assets/mock-data-json/voucher.json'
+
+    );
+
   }
+
 }
