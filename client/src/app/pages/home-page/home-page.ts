bed7812663eded3927710af8e9f3aaae4ea5e65f
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2026-05-27 01:59:11 +0700

    giao diện homepage

--- client/src/app/pages/home-page/home-page.ts
+++ client/src/app/pages/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,9 +6,33 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {
+export class HomePage implements AfterViewInit, OnInit, OnDestroy {
+  @ViewChild('homeVideo') homeVideo!: ElementRef<HTMLVideoElement>;
 
   currentSlide = 0;
+
+  banners: string[] = ['/banner1.png', '/banner2.png', '/banner3.png', '/banner4.png'];
+  currentBannerIndex = 0;
+  bannerInterval: any;
+
+  ngOnInit() {
+    this.bannerInterval = setInterval(() => {
+      this.currentBannerIndex = (this.currentBannerIndex + 1) % this.banners.length;
+    }, 2000);
+  }
+
+  ngOnDestroy() {
+    if (this.bannerInterval) {
+      clearInterval(this.bannerInterval);
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.homeVideo && this.homeVideo.nativeElement) {
+      this.homeVideo.nativeElement.muted = true;
+      this.homeVideo.nativeElement.play().catch(error => console.log('Autoplay prevented:', error));
+    }
+  }
 
   nextSlide(): void {
     const maxSlide = 1;
