19024c19e21ad493217d5ce1bab80ac7d72830c8
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2026-05-30 00:44:17 +0700

    Prepare backend for deploy

--- my-server/index.js
+++ my-server/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 // Connect DB
 const db = require('./config/db');
