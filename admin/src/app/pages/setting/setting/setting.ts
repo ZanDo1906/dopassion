cf05d7d8ebdbe4714c141a731bccd4d712e25bd5
nhan1665 <nhan10042020@gmail.com>
2026-06-02 15:44:13 +0700

    Fix bug -nhan

--- admin/src/app/pages/setting/setting/setting.ts
+++ admin/src/app/pages/setting/setting/setting.ts
@@ -3,36 +3,36 @@ import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-selector:'app-setting',
-standalone:true,
-imports:[
-CommonModule,
-NgFor,
-FormsModule
-],
-templateUrl:'./setting.html',
-styleUrls:['./setting.css']
+    selector: 'app-setting',
+    standalone: true,
+    imports: [
+        CommonModule,
+        NgFor,
+        FormsModule
+    ],
+    templateUrl: './setting.html',
+    styleUrls: ['./setting.css']
 })
 
 export class Setting {
 
-configData = [
-{
-tenThietLap:'Thời gian cho phép đợi thanh toán',
-giaTri:10,
-donVi:'Phút'
-},
-{
-tenThietLap:'Thời gian hủy đăng ký cho 1 lớp học đợi thanh toán',
-giaTri:15,
-donVi:'Phút'
-},
-{
-tenThietLap:'Khoản phí ghi danh (% học phí)',
-giaTri:20,
-donVi:'%'
-},
+    configData = [
+        {
+            tenThietLap: 'Thời gian cho phép đợi thanh toán',
+            giaTri: 10,
+            donVi: 'Phút'
+        },
+        {
+            tenThietLap: 'Thời gian hủy đăng ký cho 1 lớp học đợi thanh toán',
+            giaTri: 10,
+            donVi: 'Phút'
+        },
+        // {
+        // tenThietLap:'Khoản phí ghi danh (% học phí)',
+        // giaTri:20,
+        // donVi:'%'
+        // },
 
-];
+    ];
 
 }
